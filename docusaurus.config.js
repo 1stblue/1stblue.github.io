de8729072078fb66ab4a856cc1a1f0d2911df673
aleafs <zhangxc83@gmail.com>
2023-09-13 11:17:08 +0800

    格式调整

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,159 +6,166 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '1stblue.com',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+    title: '1stblue.com',
+    tagline: 'Dinosaurs are cool',
+    favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://1stblue.github.io',
-  // www.1stblue.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: 'https://1stblue.github.io',
+    // www.1stblue.com',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: '1stblue', // Usually your GitHub org/user name.
-  projectName: 'www.1stblue.com', // Usually your repo name.
-  trailingSlash: false,
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: '1stblue', // Usually your GitHub org/user name.
+    projectName: 'www.1stblue.com', // Usually your repo name.
+    trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'zh-Hans',
-    locales: ['en', 'zh-Hans'],
-  },
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'zh-Hans',
+        locales: ['en', 'zh-Hans'],
+    },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/1stblue.com/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/1stblue.com/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'Elastic Pipe',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type : 'docsVersionDropdown',
-            position: 'left',
-            label: '产品',
-            dropdownActiveClassDisabled: false,
-            dropdownItemsAfter: [
-              {
-                href: '/page-cloud',
-                label: 'Elastic Pipe - Cloud',
-              },
-              {
-                href: '/page-enterprise',
-                label: 'Elastic Pipe - Enterprise',
-              },
-              {
-                href: '/page-partner',
-                label: 'Elastic Pipe - Partner',
-              },
-              {
-                href: '/page-developer',
-                label: 'Elastic Pipe - Developer',
-              }
-            ]
-          },
-          {to: '/blog', label: '案例', position: 'left'},
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: '文档',
-          },
-          {to: '/docs', label: 'APIs', position: 'left'},
-          {to: '/blog', label: '博客', position: 'left'},
-          {to: '/blog', label: '活动', position: 'left'},
-          {
-            href: 'https://github.com/1stblue.com',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/1stblue',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} 1stblue.com. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        ({
+            // Replace with your project's social card
+            image: 'img/docusaurus-social-card.jpg',
+            navbar: {
+                title: 'Elastic Pipe',
+                logo: {
+                    alt: 'My Site Logo',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        type: 'docsVersionDropdown',
+                        position: 'left',
+                        label: '产品',
+                        dropdownActiveClassDisabled: false,
+                        dropdownItemsAfter: [
+                            {
+                                href: '/page-cloud',
+                                label: 'Elastic Pipe - Cloud',
+                            },
+                            {
+                                href: '/page-enterprise',
+                                label: 'Elastic Pipe - Enterprise',
+                            },
+                            {
+                                href: '/page-partner',
+                                label: 'Elastic Pipe - Partner',
+                            },
+                            {
+                                href: '/page-developer',
+                                label: 'Elastic Pipe - Developer',
+                            }
+                        ]
+                    },
+                    {to: '/blog', label: '方案', position: 'left'},
+                    {to: '/blog', label: '案例', position: 'left'},
+                    {to: '/blog', label: '连接器', position: 'left'},
+                    {
+                        type: 'docSidebar',
+                        sidebarId: 'tutorialSidebar',
+                        position: 'left',
+                        label: '文档',
+                    },
+                    {to: '/docs', label: 'APIs', position: 'left'},
+                    {to: '/blog', label: '博客', position: 'left'},
+                    {to: '/blog', label: '活动', position: 'left'},
+                    {
+                        href: 'https://github.com/1stblue',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                    {
+                        to: '/try',
+                        label: '免费试用',
+                        position: 'right',
+                    }
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Tutorial',
+                                to: '/docs/intro',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'Stack Overflow',
+                                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                            },
+                            {
+                                label: 'Twitter',
+                                href: 'https://twitter.com/docusaurus',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'More',
+                        items: [
+                            {
+                                label: 'Blog',
+                                to: '/blog',
+                            },
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/1stblue',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} 1stblue.com. Built with Docusaurus.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
 };
 
 module.exports = config;
