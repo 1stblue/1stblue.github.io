83d085535833df9ad0187e1a233b1d160da2d97a
www-max <464711201@qq.com>
2024-09-23 12:55:18 +0800

    chore: tailwindCSS颜色字体定制

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,39 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'bp-en': 'Inconsolata'
+      },
+      fontSize: {
+        'bp-h1': '64px',
+        'bp-h2': '48px',
+        'bp-h3': '24px',
+        'bp-h4': '18px',
+        'bp-body1': '18px',
+        'bp-body2': '16px',
+        'bp-caption': '12px',
+      },
       colors: {
+        bp: {
+          blue: {
+            1: '#0165FF',
+            2: '#6F99FF',
+            3: '#A2D8FF',
+          },
+          black: '#000000',
+          gray: {
+            1: '#666666',
+            2: '#F2F7FE',
+          },
+          cyan: '#B0F2FF',
+          purple: '#B489E6',
+          'light-color': {
+            1: '#E9F6FB',
+            2: '#F8F9F4',
+            3: '#F0F8F9',
+            4: '#E8E9FB'
+          }
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
