a73abb778b23d80d5c8958d74aeea9df7626a498
www-max <464711201@qq.com>
2024-09-26 17:54:39 +0800

    fix: style

--- src/components/Navbar/Telephone.tsx
+++ src/components/Navbar/Telephone.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Telephone() {
   return (
-    <div className='ml-[384]'>
+    <div className='ml-[384px]'>
        <span>
         <Image src='/icons/telephone.svg' alt="telephone" width={116} height={17} layout="responsive"/>
        </span>
