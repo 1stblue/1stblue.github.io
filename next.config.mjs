2e16059675e322fbb57791ac6b9b7aef7248d15c
www-max <464711201@qq.com>
2024-09-30 15:57:13 +0800

    chore: fix build failed

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,14 @@ import nextra from 'nextra';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
 }
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx'
 })
  
-export default {
-  ...withNextra(nextConfig)
-}
+export default withNextra(nextConfig);
