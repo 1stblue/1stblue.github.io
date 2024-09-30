f83466c103f4f4b9a0eed0a4af0ba62b50b08e60
www-max <464711201@qq.com>
2024-09-30 15:46:55 +0800

    chore: remove output and unoptimized config

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,6 @@ import nextra from 'nextra';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
 }
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
