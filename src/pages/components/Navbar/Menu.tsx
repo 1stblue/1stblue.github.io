a19070e4d8dc9321b80f8b1184d6b8c877545b39
www-max <464711201@qq.com>
2024-10-17 21:12:25 +0800

    perf: 路由闪烁问题

--- src/pages/components/Navbar/Menu.tsx
+++ src/pages/components/Navbar/Menu.tsx
@@ -1,5 +1,5 @@
-
-import React from 'react'
+import React from 'react';
+import Link from '@docusaurus/Link';
 
 export default function Menu() {
   const List = [
@@ -23,12 +23,12 @@ export default function Menu() {
   return (
     <div className='w-[70%] flex gap-[40px]'>
       {List?.map((item, index) => (
-        <a href={item.href} className='relative text-[14px] group no-underline hover:no-underline hover:text-current' key={index}>
+        <Link to={item.href} className='relative text-[14px] group no-underline hover:no-underline hover:text-current' key={index}>
          {item?.name}
         <span className="absolute left-0 bottom-[-2px] w-full h-[1px] bg-black 
          origin-bottom scale-y-0 translate-y-[1px] transition-transform duration-200 ease-in-out
          group-hover:scale-y-100 group-hover:translate-y-0"></span>
-        </a>
+        </Link>
       ))}
     </div>
   )
